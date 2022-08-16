22c6bd14e49b8d33610d21a297a802660ff3e5da
ash lea <example@thisismyactual.email>
2022-08-16 11:47:58 -0400

    remove meandrina, switch from colmena

--- flake.nix
+++ flake.nix
@@ -2,11 +2,6 @@
   inputs = {
     nixpkgs.url = "github:ashkitten/nixpkgs/nyrina";
 
-    colmena = {
-      url = "github:zhaofengli/colmena";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     impermanence = {
       url = "github:nix-community/impermanence";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -18,22 +13,8 @@
     };
   };
 
-  outputs = { self, nixpkgs, colmena, impermanence, sops-nix, ... }: {
+  outputs = { self, nixpkgs, impermanence, sops-nix, ... }: {
     nixosConfigurations = {
-      meandrina = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-
-        modules = [
-          impermanence.nixosModules.impermanence
-          sops-nix.nixosModules.sops
-          ./meandrina
-        ];
-
-        extraModules = [
-          colmena.nixosModules.deploymentOptions
-        ];
-      };
-
       xenia = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
 
@@ -42,20 +23,8 @@
           sops-nix.nixosModules.sops
           ./xenia
         ];
-
-        extraModules = [
-          colmena.nixosModules.deploymentOptions
-          { deployment.buildOnTarget = true; }
-        ];
       };
     };
-
-    colmena = {
-      meta.nixpkgs = import nixpkgs {};
-    } // builtins.mapAttrs (name: value: {
-      nixpkgs.system = value.config.nixpkgs.system;
-      imports = value._module.args.modules;
-    }) (self.nixosConfigurations);
 
     devShells."x86_64-linux".default = import ./shell.nix {
       pkgs = nixpkgs.legacyPackages."x86_64-linux";
